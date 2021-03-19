85f02509ad4bbca0911523e5dc6073d388a572b9
ariannasienabounds <77674560+ariannasienabounds@users.noreply.github.com>
2021-03-19 10:52:54 -0400

    Create Food.js

---
+++ week10/components/Food.js
@@ -0,0 +1,14 @@
+import React, {Compnent} from "React";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Food extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    My favorite food is skirt steak with chimichurri sauce.
+                </Text>
+            </View>
+        )
+    }
+}
